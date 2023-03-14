67c42fda6b113b5a6589e15ae987a42b45a96539
Gugirotto34 <gugirotto34@gmail.com>
2023-03-14 11:33:39 -0300

    Arquivo jogador

--- jogador.py
+++ jogador.py
@@ -1,5 +1,24 @@
 # gerencia as açoes do jogador #Responsavel: Girotto
 
 class Jogador:
-    def __init__(self):
-        
+    def __init__(self,vidas,letras_usadas,letra_atual):
+        self.vidas=vidas
+        self.letras_usadas=letras_usadas
+        self.letra_atual=letra_atual
+
+    def reset_vida(self):
+        self.vidas=6
+        return self.vidas
+    def diminuir_vida(self):
+        self.vidas=self.vidas-1
+        return self.vidas
+    def usar_letra(self):
+        letra_atual=self.letra_atual
+        letras_usadas=self.letras_usadas
+        usada=False
+        if letra_atual not in(letras_usadas):
+            letras_usadas.append(letra_atual)
+        else:
+            usada=True
+        return letras_usadas,usada
+
